71e1cb1454f0694001887432cec18a84ea4dfe08
dennisongaia <77359793+dennisongaia@users.noreply.github.com>
2024-07-06 11:53:01 -0700

    Update svg styling and create separate component  for collapsible

---
+++ src/components/Resume/Experience.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import * as Collapsible from "@radix-ui/react-collapsible";
+import {ChevronDownIcon, ChevronUpIcon} from "@radix-ui/react-icons";
+
+const Experience = ({item}) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Collapsible.Root className="experience-holder" open={open} onOpenChange={setOpen}>
+            <div className="experience">
+                <div className="experience-block">
+                    <div className="experience-inner">
+                        <div className="experience-content">
+                            <div className="experience-title-wrap">
+                                <div className="experience-title">
+                                    {item.title}
+                                </div>
+                                <Collapsible.Trigger asChild>
+                                    <button>
+                                        {open ? (<ChevronUpIcon className="experience-icon"/>) : (<ChevronDownIcon className="experience-icon"/>)}
+                                    </button>
+                                </Collapsible.Trigger>
+                            </div>
+                            <div className="experience-info">
+                                        <span className="experience-company">
+                                            {item.company}
+                                        </span>
+                                <span className="experience-date">
+                                            {item.start} - {item.end}
+                                        </span>
+                            </div>
+                            <Collapsible.Content>
+                                <ul className="experience-description">
+                                    {item.description.split('\n').map((line, index) => (
+                                        <li className="experience-item" key={index} style={{ marginLeft: line.startsWith('\t') ? '20px' : '0' }}>
+                                            {line.startsWith('\t') ? line.substring(1) : line}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Collapsible.Content>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Collapsible.Root>
+    )
+}
+export default Experience
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Title from "../Title/Title"
+import { experiences } from '../../constants/constants'
+import './Resume.css'
+import Experience from "./Experience";
+
+const heading = ["Resume."]
+const subheading = `Here's a little bit about my skills & experience.`
 
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <>
+            <Title heading={heading} subheading={subheading}/>
+            {experiences.map((item) =>
+                (
+                   <Experience item={item}/>
+                )
+            )}
+        </>
     )
 }
 export default Resume
