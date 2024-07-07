8f730f5141459af4f0c433a742d202653050694b
dennisongaia <77359793+dennisongaia@users.noreply.github.com>
2024-07-06 20:21:56 -0700

    Implement resume page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Title from "../Title/Title"
-import { experiences } from '../../constants/constants'
+import { experiences, skills } from '../../constants/constants'
 import './Resume.css'
 import Experience from "./Experience";
+import Skill from "./Skill";
 
 const heading = ["Resume."]
 const subheading = `Here's a little bit about my skills & experience.`
@@ -16,6 +17,15 @@ const Resume = () => {
                    <Experience item={item}/>
                 )
             )}
+            <div className="resume-skills">
+                <div className="skills-content">
+                    {skills.map((skill, index) => (
+                        <div className={`'skills-'${index % 2 === 0 ? '1' : '2'}`}>
+                            <Skill skill={skill}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/components/Resume/Skill.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import * as Progress from '@radix-ui/react-progress';
+
+const Skill = ({ skill }) => {
+    const [progress, setProgress] = React.useState(skill.percent);
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => setProgress(100 - skill.percent), 500);
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className="skill-wrap">
+            <div className="skill-image-wrap">
+                <img src={skill.image} className="skill-image"/>
+            </div>
+            <div className="skill-info-wrap">
+                <div className="skill-title">
+                    {skill.skill}
+                </div>
+                <Progress.Root className="skill-progress" value={progress}>
+                    <Progress.Indicator
+                        className="skill-indicator"
+                        style={{ transform: `translateX(-${100 - progress}%)` }}
+                    />
+                </Progress.Root>
+            </div>
+        </div>
+    )
+}
+export default Skill
