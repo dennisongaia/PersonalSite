3812fdd14ba01ff0bef899ce21ecf1a9ee3ceb56
dennisongaia <77359793+dennisongaia@users.noreply.github.com>
2024-07-22 20:44:37 -0700

    Update recaptcha to v3 on client side

--- src/components/Contact/EmailForm.js
+++ src/components/Contact/EmailForm.js
@@ -1,68 +1,53 @@
 import React, {useRef} from 'react'
 import * as Form from '@radix-ui/react-form'
 import emailjs from '@emailjs/browser'
-import ReCAPTCHA from 'react-google-recaptcha'
 import {APP_SITE_KEY, PUBLIC_KEY, SERVICE_ID, TEMPLATE_ID} from '../../constants/keys'
 
 const wait = () => new Promise((resolve) => setTimeout(resolve, 4000));
 
 const EmailForm = ({setOpen, setToastOpen, setDescription, setTitle}) => {
     const form = useRef()
-    const recaptcha = useRef()
 
     const sendEmail = async (e) => {
         e.preventDefault();
-        const captchaValue = recaptcha.current.getValue()
-        if (!captchaValue) {
-            setTitle("Alert")
-            setDescription("Please verify the reCAPTCHA")
-            setToastOpen(true)
-            wait().then(() => {
-                setToastOpen(false);
-            });
-        } else {
-            const currentUrl = window.location.origin;
-            const verifyUrl = `${currentUrl}/verify`;
-            const res = await fetch(verifyUrl,  {
-                method: 'POST',
-                body: JSON.stringify({captchaValue}),
-                headers: {
-                    'content-type': 'application/json',
-                },
-            })
-            const data = await res.json()
-            if (data.success) {
-                emailjs
-                    .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, {
-                        publicKey: PUBLIC_KEY,
-                    })
-                    .then(
-                        () => {
-                            setTitle("Success")
-                            setDescription('Email sent successfully')
-                            setToastOpen(true)
-                        },
-                        (error) => {
-                            setTitle("Error")
-                            setDescription("Email failed to send")
-                            console.log(error.text)
-                            setToastOpen(true)
-                        },
-                    );
+        window.grecaptcha.ready(() => {
+            window.grecaptcha.execute(APP_SITE_KEY, {action: 'submit'}).then((token) => {
+                console.log('reCAPTCHA token:', token);
+                if (!token) {
+                    setTitle("Alert");
+                    setDescription("Please verify the reCAPTCHA");
+                    setToastOpen(true);
+                    wait().then(() => {
+                        setToastOpen(false);
+                    });
+                } else {
+                    // Append the token to the form
+                    const formData = new FormData(form.current);
+                    formData.append('g-recaptcha-response', token);
 
-                wait().then(() => {
-                    setToastOpen(false);
-                    setOpen(false)
-                });
-            } else {
-                setTitle("Error")
-                setDescription("reCAPTCHA validation failed")
-                setToastOpen(true)
-                wait().then(() => {
-                    setToastOpen(false);
-                });
-            }
-        }
+                    emailjs
+                        .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+                        .then(
+                            () => {
+                                setTitle("Success");
+                                setDescription('Email sent successfully');
+                                setToastOpen(true);
+                            },
+                            (error) => {
+                                setTitle("Error");
+                                setDescription("Email failed to send");
+                                console.log(error.text);
+                                setToastOpen(true);
+                            },
+                        );
+
+                    wait().then(() => {
+                        setToastOpen(false);
+                        setOpen(false);
+                    });
+                }
+            });
+        });
     };
 
     return (
@@ -117,8 +102,8 @@ const EmailForm = ({setOpen, setToastOpen, setDescription, setTitle}) => {
                         <textarea className="Textarea" required name="message" placeholder="Message"/>
                     </Form.Control>
                 </Form.Field>
-                <div className="recaptcha-container">
-                    <ReCAPTCHA ref={recaptcha} sitekey={APP_SITE_KEY}/>
+                <div className="recaptcha-badge-container">
+                    <div className="g-recaptcha-badge"></div>
                 </div>
                 <Form.Submit asChild>
                     <button className="Button">
